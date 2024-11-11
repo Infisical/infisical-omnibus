609eb65e30693fc9b4dec9cc83b05d64266258cb
akhilmhdh <akhilmhdh@gmail.com>
2024-11-11 13:49:46 +0000

    feat: made auto migration option boolean

--- files/infisical-cookbooks/infisical/attributes/default.rb
+++ files/infisical-cookbooks/infisical/attributes/default.rb
@@ -34,7 +34,7 @@ default['infisical']['infisical_core']['dir'] = '/var/opt/infisical-core/infisic
 default['infisical']['infisical_core']['log_directory'] = '/var/log/infisical/infisical-core'
 default['infisical']['infisical_core']['environment'] = 'production'
 default['infisical']['infisical_core']['env_dir'] = '/opt/infisical-core/etc/infisical_core/env'
-default['infisical']['infisical_core']['auto_migration'] = 'true'
+default['infisical']['infisical_core']['auto_migration'] = true
 
 default['infisical']['infisical_core']['ENCRYPTION_KEY'] = nil
 default['infisical']['infisical_core']['AUTH_SECRET'] = nil
