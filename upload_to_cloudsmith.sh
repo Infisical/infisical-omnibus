bf698ed9e46f788f949a9c363e76ee39c67845fa
Daniel Hougaard <daniel@infisical.com>
2025-10-06 01:40:58 +0400

    fix mkrepo

--- upload_to_cloudsmith.sh
+++ upload_to_cloudsmith.sh
@@ -19,5 +19,5 @@ done
 # regenerate RPM repository metadata with mkrepo
 if ls *.rpm 1> /dev/null 2>&1; then
     export GPG_SIGN_KEY=$GPG_SIGNING_KEY_ID
-    mkrepo "s3://$INFISICAL_BINARY_S3_BUCKET/rpm --s3-access-key-id=$AWS_ACCESS_KEY_ID --s3-secret-key=$AWS_SECRET_ACCESS_KEY --s3-region=us-east-1 --sign" --sign
+    mkrepo s3://$INFISICAL_BINARY_S3_BUCKET/rpm --s3-access-key-id="$AWS_ACCESS_KEY_ID" --s3-secret-access-key="$AWS_SECRET_ACCESS_KEY" --s3-region="us-east-1" --sign
 fi
