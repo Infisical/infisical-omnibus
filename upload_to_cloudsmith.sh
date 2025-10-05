d391bba272fa3e661553fbde7d45dea2d0b08c83
Daniel Hougaard <daniel@infisical.com>
2025-10-05 08:40:23 +0400

    fix: move to AWS artifacts apt repo

--- upload_to_cloudsmith.sh
+++ upload_to_cloudsmith.sh
@@ -1,19 +1,26 @@
 cd pkg
 
 
+# for i in *.deb; do
+#     [ -f "$i" ] || break
+#     cloudsmith push deb --republish infisical/infisical-core/any-distro/any-version $i
+# done
+
 for i in *.deb; do
     [ -f "$i" ] || break
-    cloudsmith push deb --republish infisical/infisical-core/any-distro/any-version $i
+    deb-s3 upload --bucket=$INFISICAL_BINARY_S3_BUCKET --prefix=deb --visibility=private --sign=$GPG_SIGNING_KEY_ID --preserve-versions  $i
 done
 
-for i in *.rpm; do
-    [ -f "$i" ] || break
-    # stripe of amazon2023
-    new_name=$(echo "$i" | sed 's/.amazon2023//g')
-    # rename
-    if [ "$i" != "$new_name" ]; then
-        mv "$i" "$new_name"
-    fi
 
-    cloudsmith push rpm --republish infisical/infisical-core/any-distro/any-version "$new_name"
-done
+# todo(daniel): uncomment and move to AWS
+# for i in *.rpm; do
+#     [ -f "$i" ] || break
+#     # stripe of amazon2023
+#     new_name=$(echo "$i" | sed 's/.amazon2023//g')
+#     # rename
+#     if [ "$i" != "$new_name" ]; then
+#         mv "$i" "$new_name"
+#     fi
+
+#     cloudsmith push rpm --republish infisical/infisical-core/any-distro/any-version "$new_name"
+# done
