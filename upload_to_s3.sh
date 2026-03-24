d4037c12543058c890b706d1b3cb09e0c2f61d34
= <akhilmhdh@gmail.com>
2026-03-24 19:08:56 +0530

    feat: added dummpy path

--- upload_to_s3.sh
+++ upload_to_s3.sh
@@ -1,9 +1,9 @@
 cd pkg
 
-for i in *.deb; do
-    [ -f "$i" ] || break
-    deb-s3 upload --bucket=$INFISICAL_BINARY_S3_BUCKET --prefix=deb --visibility=private --sign=$GPG_SIGNING_KEY_ID --preserve-versions  $i
-done
+# for i in *.deb; do
+#     [ -f "$i" ] || break
+#     deb-s3 upload --bucket=$INFISICAL_BINARY_S3_BUCKET --prefix=deb --visibility=private --sign=$GPG_SIGNING_KEY_ID --preserve-versions  $i
+# done
 
 for i in *.rpm; do
     [ -f "$i" ] || break
@@ -17,11 +17,11 @@ for i in *.rpm; do
     #sign the rpm package
     rpmsign --addsign --key-id="$GPG_SIGNING_KEY_ID" "$new_name"
     
-    aws s3 cp "$new_name" "s3://$INFISICAL_BINARY_S3_BUCKET/rpm/Packages/"
+    aws s3 cp "$new_name" "s3://$INFISICAL_BINARY_S3_BUCKET/dummy/rpm/Packages/"
 done
 
 
 # regenerate RPM repository metadata with rpmrepo-update
 if ls *.rpm 1> /dev/null 2>&1; then
-    rpmrepo-update --backend s3 --repo-root s3://$INFISICAL_BINARY_S3_BUCKET/rpm --s3-region us-east-1 --sign-repodata --gpg-key "$GPG_SIGNING_KEY_ID" add *.rpm
+    rpmrepo-update --backend s3 --repo-root s3://$INFISICAL_BINARY_S3_BUCKET/dummy/rpm --s3-region us-east-1 --sign-repodata --gpg-key "$GPG_SIGNING_KEY_ID" add *.rpm
 fi
